54909f2511e5a59e4351b1baad9ec21111a38855
BHAUMIK PANCHAL <84496973+bhaumikpanchall@users.noreply.github.com>
2023-09-10 06:34:06 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 4213
+let accountEmail = "bhaumikpanchal21@gmail.com"
+var accountPassword = "12345678"
+accountCity = "Ahmedabad"
+
+//accountId = 2
+console.log(accountId);
+
+accountEmail = "kushpanchal21@gmail.com"
+accountPassword = "87654321"
+accountCity = "Surat"
+console.table([accountId, accountEmail, accountPassword, accountCity]);
