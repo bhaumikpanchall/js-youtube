6f556ee4ec72a6a06c8bc97441ad5d1769c393b2
BHAUMIK PANCHAL <84496973+bhaumikpanchall@users.noreply.github.com>
2023-09-10 06:53:20 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version.
+
+let name = "Bhaumik"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean => true/false
+// null =>. standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
